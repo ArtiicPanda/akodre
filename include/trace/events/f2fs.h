a1409468b0ccc3b3c540ff544b68932de4c2cbf4
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-09-21 22:37:30 +0800

    f2fs: fix up f2fs_lookup tracepoints
    [ Upstream commit 70a9ac36ffd807ac506ed0b849f3e8ce3c6623f2 ]
    
    Fix up a misuse that the filename pointer isn't always valid in
    the ring buffer, and we should copy the content instead.
    
    Fixes: 0c5e36db17f5 ("f2fs: trace f2fs_lookup")
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -804,7 +804,7 @@ TRACE_EVENT(f2fs_lookup_start,
 	TP_STRUCT__entry(
 		__field(dev_t,	dev)
 		__field(ino_t,	ino)
-		__string(name, dentry->d_name.name)
+		__string(name,	dentry->d_name.name)
 		__field(unsigned int, flags)
 	),
 
@@ -831,7 +831,11 @@ TRACE_EVENT(f2fs_lookup_end,
 	TP_STRUCT__entry(
 		__field(dev_t,	dev)
 		__field(ino_t,	ino)
+<<<<<<< HEAD
 		__string(name, dentry->d_name.name)
+=======
+		__string(name,	dentry->d_name.name)
+>>>>>>> 7727659e45f86... f2fs: fix up f2fs_lookup tracepoints
 		__field(nid_t,	cino)
 		__field(int,	err)
 	),
